e7fbd5f3aee8138074dc8b0999ca683a9e13168b
Robin Gower <robin.git@infonomics.ltd.uk>
2018-11-10 16:28:32 +0100

    Distinguish block origin and position

--- core/src/com/github/ddtc/Block.kt
+++ core/src/com/github/ddtc/Block.kt
@@ -8,11 +8,11 @@ import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.physics.box2d.BodyDef
 import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.physics.box2d.PolygonShape
+import com.github.ddtc.CandleGame.Companion.PIXELS_TO_METERS
 
 class Block(
         private val texture: Texture,
-        x: Float,
-        y: Float,
+        position: Vector2,
         world: World
 ) {
     val sprite: Sprite
@@ -20,17 +20,18 @@ class Block(
     val box: PolygonShape
     val blockWidth = 32f
     val blockHeight = 32f
-
+    val origin = Vector2(position.x + blockWidth/2, position.y+blockHeight/2)
 
     init {
         sprite = Sprite(texture)
-        sprite.setPosition(x, y)
+        sprite.setPosition(position.x, position.y)
+        sprite.setOriginCenter()
 
         val bodyDef = BodyDef()
-        bodyDef.position.set(Vector2(x,y))
+        bodyDef.position.set(origin.x/ PIXELS_TO_METERS, origin.y/PIXELS_TO_METERS)
         body = world.createBody(bodyDef)
         box = PolygonShape()
-        box.setAsBox(blockWidth, blockHeight)
+        box.setAsBox(blockWidth/2/ PIXELS_TO_METERS, blockHeight/2/ PIXELS_TO_METERS)
         body.createFixture(box, 0.0f);
         box.dispose();
     }
--- core/src/com/github/ddtc/Map.kt
+++ core/src/com/github/ddtc/Map.kt
@@ -3,6 +3,7 @@ package com.github.ddtc
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.Vector2
 
 class Map(
         private val texture: Texture,
@@ -22,7 +23,7 @@ class Map(
 
     fun loadBlocks() {
         for(i in 1..16) {
-            blocks.add(Block(texture, i*32f, 10f, world))
+            blocks.add(Block(texture, Vector2(i*32f, 10f), world))
         }
     }
 }
